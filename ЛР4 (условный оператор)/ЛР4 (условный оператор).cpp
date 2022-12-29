b19bd247b2434613d651fcc3602d55b2226ea5f5
t13rm4in <119757490+t13rm4in@users.noreply.github.com>
2022-12-29 19:35:30 +0300

    Добавьте файлы проекта.

---
+++ ЛР4 (условный оператор)/ЛР4 (условный оператор).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b;
+	cout << "1 числo (A):";
+	cin >> a;
+	cout << "2 числo (B):";
+	cin >> b;
+	if (a > b) {
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		cout << "A: " << a << ' ' << "B: " << b;
+	}
+	else {
+		cout << "A: " << a << ' ' << "B: " << b;
+	}
+}
